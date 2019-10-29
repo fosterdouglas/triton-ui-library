5dad0a659833bca2e3fef73277aa6f616d34e722
Foster <foster@sandcastle.co>
2019-10-29 04:58:26 -0700

    v0.3.5

--- styleguide.config.js
+++ styleguide.config.js
@@ -51,7 +51,7 @@ module.exports = {
     // }
     defaultExample: true,
 
-    version: 'v0.3.2',
+    version: 'v0.3.5',
     pagePerSection: true,
     usageMode: 'expand',
     styleguideDir: 'triton-ui-component-library',
